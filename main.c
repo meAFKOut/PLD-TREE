dbf96444f176cd868069f6a8d1868d66237cb63f
meAFKOut <meafkoutback@gmail.com>
2024-12-19 11:04:53 +0400

    8748

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdui.h>
+#include <stdlib.h>
+#include "binary_tree.h"
+
+
+int main() {
+
+	struct TreeNode *root = NULL;
+	int value;
+	int height;
+	int *arr = NULL ;
+	int index = 0;
+	int arr_size = 0;
+
+	printf("Write the value for Tree (Write -1 to stop):\n");
+	while(1){ 
+		printf("Write the number: ")
+		scanf("%d" , 
+	
+	
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
